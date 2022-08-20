3c5b874f3997c39ef07693482b9c09351ab2746d
yzkim9501 <yzkim9501@naver.com>
2022-08-20 17:04:27 +0900

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
   development: {
     username: process.env.DB_USER,
@@ -6,21 +6,21 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_END_POINT,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialect: "mysql",
     logging: false,
   },
   test: {
-    username: 'root',
+    username: "root",
     password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
   production: {
-    username: 'root',
+    username: "root",
     password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
 };
