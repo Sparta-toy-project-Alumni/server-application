879cba9918d3e5c7f54e52691c5e8b65e15caea3
yzkim9501 <yzkim9501@naver.com>
2022-08-20 18:20:19 +0900

    [feat] 자유게시판 글 수정, 삭제 추가

--- routes/free.js
+++ routes/free.js
@@ -40,7 +40,7 @@ router.get("/post/:freeId", async (req, res) => {
   const freePost = await Free.findOne({ where: { freeId } });
   console.log(freePost);
   if (freePost == null) {
-    res.status(200).send({
+    res.status(400).send({
       ok: false,
       message: "게시글이 존재하지 않습니다.",
     });
@@ -50,5 +50,78 @@ router.get("/post/:freeId", async (req, res) => {
     ok: true,
     freePost,
   });
+});
+
+router.get("/post/:freeId/view_count", async (req, res) => {
+  try {
+    const { freeId } = req.params;
+    const result = await Free.increment(
+      { viewCount: +1 },
+      { where: { freeId } }
+    );
+    console.log(result);
+    res.status(200).send({
+      ok: true,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).send({
+      ok: false,
+      message: `${err} : 조회수 증가 실패`,
+    });
+  }
+});
+
+router.put("/post/:freeId", async (req, res) => {
+  try {
+    const { freeId } = req.params;
+    const { title, content } = req.body;
+    console.log(freeId);
+    let post = {};
+    if (title) post.title = title;
+    if (content) post.content = content;
+    await Free.update(post, {
+      where: { freeId },
+    });
+
+    const newPost = await Free.findOne({ where: { freeId } });
+    console.log(newPost);
+    if (newPost == null) {
+      res.status(400).send({
+        ok: false,
+        message: "게시글이 존재하지 않습니다.",
+      });
+      return;
+    }
+    res.status(200).send({
+      ok: true,
+      newPost,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).send({
+      ok: false,
+      message: `${err} : 게시글 수정 실패`,
+    });
+  }
+});
+
+router.delete("/post/:freeId", async (req, res) => {
+  try {
+    const { freeId } = req.params;
+    await Free.destroy({
+      where: { freeId },
+    });
+
+    res.status(200).send({
+      ok: true,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).send({
+      ok: false,
+      message: `${err} : 게시글 삭제 실패`,
+    });
+  }
 });
 module.exports = router;
